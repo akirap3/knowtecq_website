d67c7f49fee2a4f1356608701e1347814f3945f6
akirapf3 <akirapf3@gmail.com>
2020-03-11 14:57:25 +0800

    customized contents

--- server.py
+++ server.py
@@ -11,13 +11,6 @@ def my_home():
 def html_page(page_name):
 	return render_template(page_name)
 
-def write_to_file(data):
-	with open('database.txt', mode='a') as database:
-		email = data["email"]
-		subject = data["subject"]
-		message = data["message"]
-		file = database.write(f'\n{email},{subject},{message}')
-
 def write_to_csv(data):
 	with open('database.csv', mode='a', newline='') as database2:
 		email = data["email"]
@@ -28,15 +21,15 @@ def write_to_csv(data):
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    if request.method == 'POST':
-     try:
-      data = request.form.to_dict()
-      write_to_csv(data)
-      return redirect('/thank_you.html')
-     except:
-      return 'Did not save to database'
-    else:
-    	return 'Something went wrong. Try again!'
+	if request.method=='POST':
+		try:
+			data = request.form.to_dict()
+			write_to_csv(data)
+			return redirect('thank_you.html')
+		except:
+			return 'Did not save to database'
+	else:
+		return 'Somthing went wrong, please try again'
 
 
 
